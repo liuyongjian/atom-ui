a8250c30f2aac32af8dd8515882eabc7f7c888c5
liuyongjian <1650572262@qq.com>
2024-12-18 22:43:24 +0800

    feat: 添加 TypeScript 类型声明和构建功能支持

---
+++ global.d.ts
@@ -0,0 +1,4 @@
+declare module 'component/__styles/index.css' {
+    const content: string;
+    export default content;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path"
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -6,7 +6,27 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "component": path.resolve(__dirname, "./components"),
+      "component": path.resolve(__dirname, "./components"), // 保留别名配置
+    },
+  },
+  css: {
+    postcss: './postcss.config.js',
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'components/index.ts'),  // 指定组件库的入口文件
+      name: 'AtomUI',  // 设置库的全局变量名
+      fileName: (format) => `atom-ui.${format}.js`,  // 输出文件名
+    },
+    rollupOptions: {
+      // 确保外部化依赖项，不将 React 和 React DOM 打包进库
+      external: ['react', 'react-dom', 'tailwindcss'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
 })
